f593454918e904b4b296558765c6423c0d6e1771
Cristian Arenas <programador2@duquesa.com.co>
2020-05-07 15:22:59 -0500

    07 de Mayo del 2020 v2

--- resources/views/empleados/create.blade.php
+++ resources/views/empleados/create.blade.php
@@ -1,76 +1,108 @@
-
 @extends('layouts.app')
 @section('content')
-{!! Form::close() !!}
+
+
+<div class="container">
+
+	<div class="row justify-content-center">
+		  <div class="col-md-8">
+	<div class="panel-body">
+		<div class="panel-heading">
+{!! Form::open(['route' => 'empleados.store', 'method'=>'POST','id'=>'reg_form2']) !!}
+
+
 {!! Form::hidden('CODCC', 99999, []) !!}
 {!! Form::hidden('NOMBRECOSTOS', 'EXTERNO', []) !!}
 			
 			<h3 class="panel-title">Registro Externo</h3>
-		1
-		</div>
-		<div class="panel-body">
-		<div class="panel-heading">
-	</div>
-	</div>
-	</div>
-	</div>
+		
+
+
 	<div class="panel panel-primary">
-	<label for="id">Apellidos</label>
-	<label for="id">ARL</label>
-	<label for="id">Cédula'7856U/
-	<label for="id">Dirección</label>
-	<label for="id">Email</label>
-	<label for="id">Empresa</label>
-	<label for="id">EPS</label>
-	<label for="id">Fecha de Nacimiento</label>
-	<label for="id">Género</label>
-	<label for="id">Nombre</label>
-	<label for="id">Téléfono:</label>
-	{!! Form::date('FECNAC', null,['class' => 'form-control', 'placeholder' => 'FECNAC','name'=>'FECNAC', 'required']) !!}
-	{!! Form::email('email', null,['class' => 'form-control', 'placeholder' => 'Email','name'=>'email', 'required']) !!}
-	{!! Form::select('SEXO',[ ''=>'Seleccione...','M'=>'Masculino', 'F' =>'Femenino'],null,['class'=> 'form-control','name'=>'SEXO', 'required'] )!!}
-	{!! Form::text('APELLIDO', null,['class' => 'form-control', 'placeholder' => 'Digite los Apellidos','name'=>'APELLIDO', 'required']) !!}
-	{!! Form::text('ARL', null,['class' => 'form-control', 'placeholder' => 'Digite la ARL','name'=>'ARL', 'required']) !!}
+
+	
+	<div class="form-group">
+	<label for="id">Cédula</label>
 	{!! Form::text('CEDULA', null,['class' => 'form-control', 'placeholder' => 'Digite la Cédula','name'=>'CEDULA', 'required']) !!}
-	{!! Form::text('DIRECCION', null,['class' => 'form-control', 'placeholder' => 'Digite la dirección','name'=>'DIRECCION', 'required']) !!}
-	{!! Form::text('EMPRESA', null,['class' => 'form-control', 'placeholder' => 'Digite la empresa donde laborá','name'=>'EMPRESA']) !!}
-	{!! Form::text('EPS', null,['class' => 'form-control', 'placeholder' => 'Digite la EPS','name'=>'EPS', 'required']) !!}
+	</div>
+
+	<div class="form-group">
+	<label for="id">Nombre: </label>
 	{!! Form::text('NOMBRE', null,['class' => 'form-control', 'placeholder' => 'Nombre completo','name'=>'NOMBRE', 'required']) !!}
+	</div>
+
+	<div class="form-group">
+	<label for="id">Apellido: </label>
+	{!! Form::text('APELLIDO', null,['class' => 'form-control', 'placeholder' => 'Digite los Apellidos','name'=>'APELLIDO', 'required']) !!}
+	</div>
+
+		<div class="form-group">
+	<label for="id">Teléfono: </label>
 	{!! Form::text('TELEFONO', null,['class' => 'form-control', 'placeholder' => 'Digite el teléfono','name'=>'TELEFONO', 'required']) !!}
+	</div>
+
+			<div class="form-group">
+	<label for="id">Dirección: </label>
+	{!! Form::text('DIRECCION', null,['class' => 'form-control', 'placeholder' => 'Digite la dirección','name'=>'DIRECCION', 'required']) !!}
+	</div>
+
+				<div class="form-group">
+	<label for="id">Fecha de Nacimiento: </label>
+	{!! Form::date('FECNAC', null,['class' => 'form-control', 'placeholder' => 'FECNAC','name'=>'FECNAC', 'required']) !!}
+	</div>
+
+				<div class="form-group">
+	<label for="id">Empresa donde laborá: </label>
+	{!! Form::text('EMPRESA', null,['class' => 'form-control', 'placeholder' => 'Digite la empresa donde laborá','name'=>'EMPRESA']) !!}
+
+	</div>
+
+
+				<div class="form-group">
+	<label for="id">Correo Electrónico: </label>
+	{!! Form::email('email', null,['class' => 'form-control', 'placeholder' => 'Email','name'=>'email', 'required']) !!}
+
+	</div>
+
+				<div class="form-group">
+	<label for="id">Género: </label>
+	{!! Form::select('SEXO',[ ''=>'Seleccione...','M'=>'Masculino', 'F' =>'Femenino'],null,['class'=> 'form-control','name'=>'SEXO', 'required'] )!!}
+	</div>
+
+					<div class="form-group">
+	<label for="id">ARL: </label>
+	{!! Form::text('ARL', null,['class' => 'form-control', 'placeholder' => 'Digite la ARL','name'=>'ARL', 'required']) !!}
+	</div>
+
+	<div class="form-group">
+	<label for="id">EPS: </label>
+	{!! Form::text('EPS', null,['class' => 'form-control', 'placeholder' => 'Digite la EPS','name'=>'EPS', 'required']) !!}
+	</div>
+
+
+   
+    <center><button type="submit" class="btn btn-block btn-primary" >Enviar</button>
+	
+	</div>
+	</div>
+	</div>
+	</div>
+	
+	
+	
+	
+	
+	
+	
    
     
      
                                              
-    </div> -->
-    <button type="reset" class="btn btn-danger">Borrar</button></center><p>
-    <center><button type="submit" class="btn btn-primary" >Enviar</button>
-    <label for="id">Centro de costos</label>
-  <div class="col-md-8">
- <!--    <div class="form-group">
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>     
-<div class="container">
-<div class="form-group" >
-<div class="form-group">
-<div class="form-group">
-<div class="form-group">
-<div class="form-group">
-<div class="form-group">
-<div class="form-group">
-<div class="form-group">
-<div class="form-group">
-<div class="form-group">
-<div class="form-group">
-<div class="row justify-content-center">
+ 
+ 
+    
+
+
+  	{!! Form::close() !!}
+
 @endsection
